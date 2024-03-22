48ab6dc10c5035c848cff1b9127006abe1f0eaa8
Brayden Wong <Braydenw@eagltechnology.com>
2024-03-22 11:26:01 -0600

    added more comments and removed a console.log

--- apps/react/src/app/page.tsx
+++ apps/react/src/app/page.tsx
@@ -2,14 +2,15 @@ import { api } from "libs";
 import { LevelSelector } from "./components/level-selector";
 
 export default async function MainPage() {
+  /* Load data from the backend */
   const { data: levels, error } = await api.levels.get();
 
+  /* Handle loading and error states */
   if (error) {
     return <div>Failed to load levels</div>;
   }
 
-  console.log(levels);
-
+  /* Returns JSX that shows the level selector */
   return (
     <main className="h-screen overflow-hidden flex flex-col space-y-2">
       <header className="shrink-0 border-b-2 border-black py-2 text-2xl px-4">
